454135a4294f32b74e33a7afc2d0b3f73ecbef31
mac <harryzhangpro@gmail.com>
2025-04-17 09:55:15 +0800

    modified:   .gitignore 	new file:   cal2.txt 	new file:   dh.txt 	modified:   spheres.py

--- spheres.py
+++ spheres.py
@@ -179,7 +179,7 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="逐帧拟合球体并评估误差")
     parser.add_argument("--base_dir", type=str, default="./0411", help="点云文件夹路径")
     parser.add_argument("--robot_file", type=str, default="aubo_record_2025-04-11_22-15-34.txt", help="机械臂位姿文件")
-    parser.add_argument("--calib_file", type=str, default="cal3.txt", help="手眼标定文件")
+    parser.add_argument("--calib_file", type=str, default="cal2.txt", help="手眼标定文件")
     args = parser.parse_args()
 
     main(args.base_dir, 
